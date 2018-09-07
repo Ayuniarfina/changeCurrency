a5713391a3963780326faa4c91c86b9377fa170b
ayuni arfina <ayuniarfina06@gmail.com>
2018-09-07 20:52:04 +0700

    changeCurrency

--- currency/src/App.js
+++ currency/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Card, CardGroup, CardHeader, CardText, CardBlock, Col, Row } from 'reactstrap';
+import { Card, CardGroup, CardHeader, CardText, CardBlock, Col, Row, InputGroupAddon, Button, Input, InputGroup } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      inputValue: ''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -20,6 +22,10 @@ class App extends Component {
           })
       }
 
+  handleChange(event){
+    this.setState({inputValue: event.target.value})
+  }
+
   render() {
     const symbols = [
           {"curency" : "CAD", "country" : "Canadian Dollar"}, 
@@ -31,14 +37,21 @@ class App extends Component {
           {"curency" : "MYR", "country" : "Malaysian Ringgit"},
           {"curency" : "JPY", "country" : "Japanese Yen"},
           {"curency" : "KRW", "country" : "South Korean won"}
-        ]
+        ];
 
     return (
       <div>
         <Row className="justify-content-center">
           <Col sm="6" style={{ marginTop: '2rem' }}> 
             <Card>
-              <CardHeader>Title</CardHeader>
+              <CardHeader>
+              <InputGroup style={{paddingBottom: '0.6rem'}}>
+              <Input type="number" value={this.state.inputValue} onChange={this.handleChange} placeholder="input your change here"/>
+              <InputGroupAddon addonType="prepend"></InputGroupAddon>
+              </InputGroup>
+              <h6><i>USD - United States Dollars</i></h6>
+              <b>USD</b><p style={{float: 'right', clear: 'both'}}><h3>{this.state.inputValue}.000</h3></p>
+              </CardHeader>
             </Card>
           
             { symbols.map((symbol, i) => (
@@ -46,10 +59,14 @@ class App extends Component {
                 <Col xs="6" sm="11" style={{padding:0}}>
                   <Card style={{height: '6.5vw'}}>
                     <CardBlock style={{padding:'0.6rem'}}>
-                      <CardText style={{flex: 1, flexDirection: 'row'}}>
-                        <a style={{flex: 1}}>{symbol.curency}</a>
-                        <a style={{flex: 1, textAlign: 'right'}}>{this.state.data.symbol}</a>
-                      <a className="text-right">{this.state.data.symbol}</a><h6><b><i>{symbol.country}</i></b></h6><i>1 USD = {symbol.curency} {this.state.data[symbol.curency]}</i></CardText>
+                      <CardText>
+                        <a>{symbol.curency}</a>
+                        <p style={{float: 'right', clear: 'both'}}><h3>{((this.state.data[symbol.curency])*(this.state.inputValue)).toFixed(4)}</h3></p>
+                        <a>{this.state.data.symbol}</a>
+                        <a className="text-right">{this.state.data.symbol}</a>
+                        <h6><b><i>{symbol.country}</i></b></h6>
+                        <i>1 USD = {symbol.curency} {this.state.data[symbol.curency]}</i>
+                      </CardText>
                     </CardBlock>
                   </Card>
                 </Col>
@@ -63,7 +80,7 @@ class App extends Component {
                   </div>
                 </Col>
               </CardGroup>
-            ))};
+            ))}
 
           </Col>  
         </Row>
